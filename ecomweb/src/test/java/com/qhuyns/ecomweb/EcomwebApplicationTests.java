b2148982765cb79ec43fd2470341547aab38ca84
youngboi_seetjnk <quanghuytq3011@gmail.com>
2025-06-22 18:39:32 +0700

    update

---
+++ ecomweb/src/test/java/com/qhuyns/ecomweb/EcomwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qhuyns.ecomweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
