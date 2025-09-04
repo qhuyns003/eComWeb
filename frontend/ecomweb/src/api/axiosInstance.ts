72b7fc472a92f977ad497c795637aa73e10dfb6c
youngboi_seetjnk <143262781+qhuyns003@users.noreply.github.com>
2025-09-04 19:45:07 +0700

    Update axiosInstance.ts

--- frontend/ecomweb/src/api/axiosInstance.ts
+++ frontend/ecomweb/src/api/axiosInstance.ts
@@ -9,23 +9,23 @@ const axiosInstance = axios.create({
 
 //interceptor Thêm/xử lý logic chung cho tất cả các API call mà không cần lặp lại code ở từng chỗ.
 
-// // Thêm interceptor cho response để xử lý token hết hạn
-// axiosInstance.interceptors.response.use(
-//     (response) => response,
-//     (error) => {
-//         if (error.response && (error.response.status === 401 || error.response.status === 403)) {
-//             // Xóa token khỏi localStorage
-//             localStorage.removeItem('token');
-            
-//             // Dispatch custom event để component khác có thể xử lý
-//             const logoutEvent = new CustomEvent('tokenExpired', {
-//                 detail: { message: 'Phiên đăng nhập đã hết hạn' }
-//             });
-//             window.dispatchEvent(logoutEvent);
-//         }
-//         return Promise.reject(error);
-//     }
-// );
+// Thêm interceptor cho response để xử lý token hết hạn
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        const token = localStorage.getItem('token');
+        if (token && error.response && error.response.status === 401) {
+            // Xóa token khỏi localStorage
+            localStorage.removeItem('token');
+            // Dispatch custom event để component khác có thể xử lý
+            const logoutEvent = new CustomEvent('tokenExpired', {
+                detail: { message: 'Phiên đăng nhập đã hết hạn' }
+            });
+            window.dispatchEvent(logoutEvent);
+        }
+        return Promise.reject(error);
+    }
+);
 
 // Thêm interceptor cho request để tự động gắn token nếu có
 axiosInstance.interceptors.request.use(
